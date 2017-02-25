e75a1412fce7161214c1519eef1a3a4f72b27f12
zhengxiaohu <1050087728@qq.com>
2017-02-25 21:51:15 +0800

    Add ListView with custom colors

---
+++ app/src/main/java/cn/edu/buaa/a103/reminders/RemindersActivity.java
@@ -0,0 +1,25 @@
+package cn.edu.buaa.a103.reminders;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class RemindersActivity extends AppCompatActivity {
+
+    private ListView mListView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reminders);
+
+        mListView= (ListView) findViewById(R.id.lv_reminders);
+
+        //The arrayAdapter is the controller in our model-view-controller relationship.(controller)
+        ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(this,R.layout.reminders_row,R.id.row_text,
+                new String[]{"first record","second record","third record"});
+
+        mListView.setAdapter(arrayAdapter);
+    }
+}
